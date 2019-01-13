4aa32c88ee0de9d93bd5c5009a056a435a61eeb7
Robin <contact@robin-d.fr>
2019-01-13 18:03:54 +0100

    complex multiple check headers

--- src/MultipleCheckInHeaders.php
+++ src/MultipleCheckInHeaders.php
@@ -7,14 +7,22 @@ namespace PiedWeb\Curl;
  */
 class MultipleCheckInHeaders
 {
+    protected $expectedCode;
+    protected $expectedType;
     protected $code;
+
+    public function __construct(int $expectedCode = 200, string $expectedType = 'text/html')
+    {
+        $this->expectedCode = $expectedCode;
+        $this->expectedType = $expectedType;
+    }
 
     public function check($line)
     {
-        if (Helper::checkStatusCode($line)) {
+        if (Helper::checkStatusCode($line, $this->expectedCode)) {
             $this->code = 200;
         }
 
-        return 200 == $this->code && Helper::checkContentType($line);
+        return 200 == $this->code && Helper::checkContentType($line, $this->expectedType);
     }
 }
